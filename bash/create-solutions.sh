2b792751d3c3aee8e3710a87c640541008a71e23
Ibrahim El Far <ibrahimelfar@users.noreply.github.com>
2018-05-05 21:57:17 -0700

    Initial Framework for Organizing Code
    Script to generate projects
    Initial set of projects

---
+++ bash/create-solutions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -a chapters=(
+  "csharp/CrackingTheCodingInterview/_1_1/IsUnique"
+  "csharp/CrackingTheCodingInterview/_1_2/CheckPermutation"
+  "csharp/CrackingTheCodingInterview/_1_3/Urlify"
+  "csharp/CrackingTheCodingInterview/_1_4/PalindromePermutation"
+  "csharp/CrackingTheCodingInterview/_1_5/OneAway"
+  "csharp/CrackingTheCodingInterview/_1_6/StringCompression"
+  "csharp/CrackingTheCodingInterview/_1_7/RotateMatrix"
+  "csharp/CrackingTheCodingInterview/_1_8/ZeroMatrix"
+  "csharp/CrackingTheCodingInterview/_1_9/StringRotation"
+)
+
+# Create the solution
+root=csharp/CrackingTheCodingInterview
+mkdir -p $root
+pushd $root
+dotnet new sln
+popd
+
+for i in "${chapters[@]}"
+do
+  # Create the project
+  mkdir -p $i
+  name=$(basename -- "$i")
+  pushd $i
+  dotnet new console --language C#
+  popd
+
+  # Add the project to the solution
+  pushd $root
+  dotnet sln add "../../${i}/${name}.csproj"
+  popd
+done
