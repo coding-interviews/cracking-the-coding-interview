52932d8fead8cd515a6aa2cde1abaed59b8140d4
Ibrahim El Far <ibrahim_elfar@hotmail.com>
2018-05-06 09:34:54 -0700

    chapter 1.3

--- csharp/CrackingTheCodingInterview/_1_3/Urlify/Program.cs
+++ csharp/CrackingTheCodingInterview/_1_3/Urlify/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Text;
 
 namespace Urlify
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+  /// <summary>
+  /// Write a method that urlifies a string assuming it has enough space to replace each space with a %20.
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args) {
+      string[] inputs = {
+        null,
+        "",
+        " ",
+        "a",
+        "abc",
+        "a b",
+        " ab",
+        "ab ",
+        "ab c ",
+        "abc  ",
+        " abc ",
+        " a c ",
+        " x z    ",
+      };
+
+      for (int i = 0; i < inputs.Length; i++) {
+        string input = inputs[i] + "      ";
+        char[] array = input.ToCharArray();
+        Solution.Urlify(array, inputs[i] == null ? 0 : inputs[i].Length);
+
+        Console.WriteLine($"{Display(input.ToCharArray()),-16} | {Display(array),-16}");
+      }
     }
+
+    private static string Display(char[] input) {
+      if (input == null) return "<null>";
+      if (input.Length == 0) return "<empty>";
+
+      var sb = new StringBuilder();
+      foreach (var c in input) {
+        if (c == ' ') {
+          sb.Append('~');
+        } else {
+          sb.Append(c);
+        }
+      }
+
+      return sb.ToString();
+    }
+  }
 }
