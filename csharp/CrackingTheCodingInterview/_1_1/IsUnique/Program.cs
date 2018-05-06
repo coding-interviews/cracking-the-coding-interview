5f641b7fd39db3caa60c8ba8505daad665d470b8
Ibrahim El Far <ibrahim_elfar@hotmail.com>
2018-05-05 22:48:53 -0700

    1.1 solution

--- csharp/CrackingTheCodingInterview/_1_1/IsUnique/Program.cs
+++ csharp/CrackingTheCodingInterview/_1_1/IsUnique/Program.cs
@@ -2,11 +2,41 @@
 
 namespace IsUnique
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+  /// <summary>
+  /// Implement an algorithm to determine if a string has all unique characters.
+  /// </summary>
+  static class Program
+  {
+    static void Main(string[] args) {
+      string[] inputs = {
+        null,
+        String.Empty,
+        "a",
+        "ab",
+        "abc",
+        "aba",
+        "aab",
+        "abcabc"
+      };
+
+      Console.WriteLine("Input    | Output");
+      Console.WriteLine("-------- | ------");
+      foreach (string input in inputs) {
+        Console.WriteLine($"{Display(input), -8} | {Solutions.UsingASet(input)}");
+      }
     }
+
+    private static string Display(string input) {
+      switch (input) {
+        case null:
+          return "<null>";
+
+        case "":
+          return "<empty>";
+
+        default:
+          return input;
+      }
+    }
+  }
 }
