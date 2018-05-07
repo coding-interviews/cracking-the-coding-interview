6a8efbc49060b7d8dd33ab5843377cf00504e9bf
Ibrahim El Far <ibrahim_elfar@hotmail.com>
2018-05-07 13:06:25 -0700

    problem 1.7

--- csharp/CrackingTheCodingInterview/_1_7/RotateMatrix/Program.cs
+++ csharp/CrackingTheCodingInterview/_1_7/RotateMatrix/Program.cs
@@ -2,11 +2,44 @@
 
 namespace RotateMatrix
 {
-    class Program
+  static class Program
+  {
+		static int[,] GenerateMatrix(int size) {
+			var count = 1;
+			var matrix = new int[size, size];
+			for (var row = 0; row < size; row++) {
+				for (var col = 0; col < size; col++) {
+					matrix[row, col] = count++;
+				}
+			}
+
+			return matrix;
+		}
+
+		static void WriteMatrixToConsole(int[,] matrix) {
+			int size = matrix.GetLength(0);
+
+			for (var row = 0; row < size; row++)
+			{
+				for (var col = 0; col < size; col++)
+				{
+					Console.Write($"{matrix[row, col],-2} ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+			for (var size = 1; size <= 5; size++)
+			{
+				var matrix = GenerateMatrix(size);
+				WriteMatrixToConsole(matrix);
+				Console.WriteLine("----------");
+				Solution.Rotate(matrix);
+				WriteMatrixToConsole(matrix);
+				Console.WriteLine("----------");
+			}
     }
+  }
 }
---
+++ csharp/CrackingTheCodingInterview/_1_7/RotateMatrix/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace RotateMatrix
+{
+	static class Solution
+  {
+		public static void Rotate(int[,] matrix) {
+			int size = matrix.GetLength(0);
+
+			for (int layer = 0; layer < size/2; layer++) { // layer depth
+				int first = layer;
+				int last = size - layer - 1; // layer length
+
+				for (int i = first; i < last; i++) {
+					// Calculate offset
+					int offset = i - first;
+
+					// Save the top
+					int top = matrix[first, i];
+
+					// Rotate left into top
+					matrix[first, i] = matrix[last - offset, first];
+
+					// Rotate bottom into left
+					matrix[last - offset, first] = matrix[last, last - offset];
+
+					// Rotate right into bottom
+					matrix[last, last - offset] = matrix[i, last];
+
+					// Rotate saved top into right
+					matrix[i, last] = top;
+				}
+
+			}
+		}
+  }
+}
