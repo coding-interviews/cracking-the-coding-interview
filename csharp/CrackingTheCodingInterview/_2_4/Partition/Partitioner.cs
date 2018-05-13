3f9a5582d18ca07fcefe51191c302d90748caa15
Ibrahim El Far <ibrahim_elfar@hotmail.com>
2018-05-13 00:59:09 -0700

    problem 2.4

---
+++ csharp/CrackingTheCodingInterview/_2_4/Partition/Partitioner.cs
@@ -0,0 +1,51 @@
+﻿using Utilities.LinkedLists;
+
+namespace Partition
+{
+	public static class Partitioner
+  {
+		public static Node Partition(Node head, int p) {
+			if (head == null || head.Next == null) return head;
+
+			Node beforeHead = null;
+			Node beforeTail = null;
+			Node afterHead = null;
+			Node afterTail = null;
+
+			var current = head;
+			while (current != null) {
+				if (current.Data <= p) {
+					// Add it to the "before" list
+					if (beforeHead == null) {
+						beforeHead = current;
+						beforeTail = current;
+						current = current.Next;
+						beforeTail.Next = null;
+					} else {
+						beforeTail.Next = current;
+						beforeTail = current;
+						current = current.Next;
+						beforeTail.Next = null;
+					}
+				} else {
+					// Add it to the "after" list
+					if (afterHead == null) {
+            afterHead = current;
+            afterTail = current;
+            current = current.Next;
+            afterTail.Next = null;
+          } else {
+            afterTail.Next = current;
+            afterTail = current;
+            current = current.Next;
+            afterTail.Next = null;
+          }
+				}
+			}
+
+			if (beforeHead == null) return afterHead;
+			beforeTail.Next = afterHead;
+			return beforeHead;
+		}
+  }
+}
--- csharp/CrackingTheCodingInterview/_2_4/Partition/Program.cs
+++ csharp/CrackingTheCodingInterview/_2_4/Partition/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using Utilities.LinkedLists;
 
 namespace Partition
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+	class Program
+	{
+		static IEnumerable<Node> BuildLists() {
+      yield return ListBuilder.Build();
+      yield return ListBuilder.Build(1);
+			yield return ListBuilder.Build(1, 2, 3);
+			yield return ListBuilder.Build(6, 7, 8);
+			yield return ListBuilder.Build(5, 1, 2, 3);
+			yield return ListBuilder.Build(1, 5, 2, 3);
+			yield return ListBuilder.Build(1, 2, 5, 3);
+			yield return ListBuilder.Build(1, 2, 3, 5);
+			yield return ListBuilder.Build(5, 6, 7, 8);
+			yield return ListBuilder.Build(6, 5, 7, 8);
+			yield return ListBuilder.Build(6, 7, 5, 8);
+			yield return ListBuilder.Build(6, 7, 8, 5);
+			yield return ListBuilder.Build(1, 2, 3, 5, 6, 7, 8);
+			yield return ListBuilder.Build(1, 5, 6, 2, 3, 7, 8);
+			yield return ListBuilder.Build(1, 5, 2, 3, 5, 6, 7, 8, 5, 6, 5, 4, 9);
     }
+
+		static void Main(string[] args) {
+			foreach (var list in BuildLists()) {
+				Console.WriteLine($"before: {Display.List(list)}");
+				var partitioned = Partitioner.Partition(list, 5);
+				Console.WriteLine($"after:  {Display.List(partitioned)}");
+			}
+		}
+	}
 }
